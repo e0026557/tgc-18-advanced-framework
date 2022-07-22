ac8faef28a2714f08e34647160772bfc71de41d4
e0026557 <e0026557@u.nus.edu>
2022-07-22 11:25:23 +0800

    Added router function

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-const express = require('express');
-const { handlebars } = require('hbs');
-const hbs = require('hbs');
-const wax = require('wax-on');
+const express = require("express");
+const hbs = require("hbs");
+const wax = require("wax-on");
 
 const app = express();
 
-app.set('view engine', 'hbs');
+app.set("view engine", "hbs");
 
 // Static folder
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Setup wax-on
 wax.on(hbs.handlebars);
-wax.setLayoutPath('./views/layouts');
+wax.setLayoutPath("./views/layouts");
 
-app.get('/', async function(req, res) {
-  res.send('hello world');
-})
+const landingRoutes = require("./routes/landing");
+app.use("/", landingRoutes);
 
-app.listen(3000, function() {
-  console.log('Server has started');
-})
+app.listen(3000, function () {
+  console.log("Server has started");
+});
