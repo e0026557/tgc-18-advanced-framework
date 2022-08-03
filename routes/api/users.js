cc32237603410cc65c16481cd9936b306d4e5c5d
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-08-03 06:03:30 +0000

    added refresh tokens and blacklist

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,7 +4,6 @@ const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
 const { checkIfAuthenticatedJWT } = require('../../middlewares');
 
-
 const generateAccessToken = function (
   username,
   id,
@@ -33,7 +32,7 @@ const getHashedPassword = function (password) {
   return hash;
 };
 
-const { User } = require('../../models');
+const { User, BlacklistedToken } = require('../../models');
 
 router.post('/login', async function (req, res) {
   const user = await User.where({
@@ -54,8 +53,17 @@ router.post('/login', async function (req, res) {
       '1h'
     );
 
+    const refreshToken = generateAccessToken(
+      user.get('username'),
+      user.get('id'),
+      user.get('email'),
+      process.env.REFRESH_TOKEN_SECRET,
+      '7d'
+    );
+
     res.json({
-      accessToken: accessToken
+      accessToken: accessToken,
+      refreshToken: refreshToken
     });
   } else {
     // error
@@ -66,9 +74,89 @@ router.post('/login', async function (req, res) {
   }
 });
 
-router.get('/profile', checkIfAuthenticatedJWT, function(req, res) {
+router.get('/profile', checkIfAuthenticatedJWT, function (req, res) {
   const user = req.user;
   res.json(user);
+});
+
+// This route to get new access token
+router.post('/refresh', async function (req, res) {
+  // Get the refreshToken from the body (need not be in authorisation header for refresh tokens)
+  const refreshToken = req.body.refreshToken;
+
+  if (refreshToken) {
+    // Check if the token is already blacklisted
+    const blacklistedToken = await BlacklistedToken.where({
+      token: refreshToken
+    }).fetch({
+      require: false
+    });
+
+    // if the blacklistedToken is NOT null, then it means it exists and was blacklisted
+    if (blacklistedToken) {
+      res.status(400);
+      res.json({
+        error: 'Refresh token has been blacklisted'
+      })
+      return; // end function
+    }
+
+    // verify if legit
+    jwt.verify(
+      refreshToken,
+      process.env.REFRESH_TOKEN_SECRET,
+      function (err, tokenData) {
+        if (!err) {
+          // Generate new access token
+          const accessToken = generateAccessToken(
+            tokenData.username,
+            tokenData.id,
+            tokenData.email,
+            process.env.TOKEN_SECRET, // Note we are creating new access token not refresh token
+            '1h'
+          );
+
+          res.json({
+            accessToken: accessToken
+          });
+        } else {
+          res.status(400);
+          res.json({
+            error: 'Invalid refresh token'
+          });
+        }
+      }
+    );
+  } else {
+    res.status(400);
+    res.json({
+      error: 'No refresh token found'
+    });
+  }
+});
+
+router.post('/logout', async function (req, res) {
+  const refreshToken = req.body.refreshToken;
+  if (refreshToken) {
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, async function (err, tokenData) {
+      // add refresh token to black list
+      if (!err) {
+        const token = new BlacklistedToken();
+        token.set('token', refreshToken);
+        token.set('date_created', new Date());
+        await token.save();
+        res.json({
+          'message': 'Logged out'
+        })
+      }
+    })
+  }
+  else {
+    res.status(400);
+    res.json({
+      error: 'No refresh token found'
+    })
+  }
 })
 
 module.exports = router;
